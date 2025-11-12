b0f9919ea71581fc4afd95852eb775faef596b32
abduraufdev0788 <abduraufnasrullayev790@gmail.com>
2025-11-12 23:59:11 +0500

    add_product

--- seller/models.py
+++ seller/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Products(models.Model):
+    CATEGORY_CHOICES = [
+        ('food', 'Oziq-ovqat'),
+        ('drinks', 'Ichimliklar'),
+        ('clothes', 'Kiyim-kechak'),
+        ('electronics', 'Elektronika'),
+        ('appliances', 'Maishiy texnika'),
+        ('accessories', 'Aksessuarlar'),
+        ('kids', 'Bolalar uchun mahsulotlar'),
+    ]
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    rating = models.FloatField()
+    viewers = 0
+    image = models.ImageField(upload_to="images/")
+    price = models.DecimalField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    category = models.CharField(choices=CATEGORY_CHOICES)
+    discount = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}  {self.price}"
+
+
--- seller/views.py
+++ seller/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from accounts.models import Accounts
+from django.http import HttpRequest, HttpResponse
 
-# Create your views here.
+def products_list(request:HttpRequest)->HttpResponse:
+    pass
+
+def add_product(request:HttpRequest)->HttpResponse:
+    pass
